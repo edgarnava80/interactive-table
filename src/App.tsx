16060679519cd3f7c65a3fb9d66cf7fbe1d372e2
Edgar Nava <edgarnava2680@gmail.com>
2025-05-26 11:06:11 -0500

    Replace dummy data with real fetch from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,62 @@
 import { type ColumnDef } from "@tanstack/react-table"
 import type { AcademicWork } from "@/types"
 import InteractiveTable from "@/components/InteractiveTable"
-
-const data: AcademicWork[] = [
-  {
-    title: "A Deep Dive into Neural Networks",
-    publication_year: 2021,
-    cited_by_count: 150,
-    authorships: [
-      { author: { display_name: "Alice Smith" } },
-      { author: { display_name: "Bob Johnson" } },
-    ],
-  },
-  {
-    title: "Quantum Computing for Beginners",
-    publication_year: 2020,
-    cited_by_count: 98,
-    authorships: [
-      { author: { display_name: "Carlos Rivera" } },
-    ],
-  },
-  {
-    title: "Understanding GPT Models",
-    publication_year: 2023,
-    cited_by_count: 75,
-    authorships: [
-      { author: { display_name: "Dana Lee" } },
-      { author: { display_name: "Ethan Brown" } },
-      { author: { display_name: "Moe Green" } },
-    ],
-  },
-]
-
-const columns: ColumnDef<AcademicWork>[] = [
-  {
-    accessorKey: "title",
-    header: "📖 Título",
-    cell: (info) => info.getValue(),
-  },
-  {
-    accessorKey: "publication_year",
-    header: "📅 Año",
-    cell: (info) => info.getValue(),
-  },
-  {
-    accessorKey: "cited_by_count",
-    header: "🔢 Citaciones",
-    cell: (info) => info.getValue(),
-  },
-  {
-    id: "authors",
-    header: "👥 Autores",
-    cell: ({ row }) => {
-      const rawAuthors = row?.original?.authorships.length > 2 ? row.original.authorships.slice(0, 2) : row.original.authorships
-      const authors = rawAuthors.map((a) => a.author.display_name).join(", ")
-      return <span>{authors}</span>
-    },
-  },
-]
+import { useEffect, useState } from "react"
 
 function App() {
+  const [data, setData] = useState<AcademicWork[]>([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // const query = "https://api.openalex.org/works?filter=cited_by_count:100&per_page=100"
+      const query = "https://api.openalex.org/works?per_page=100"
+      try {
+        const response = await fetch(query)
+        const jsonData = await response.json()
+        setData(jsonData.results)
+        console.log(jsonData.results)
+      } catch (error) {
+        console.error("Error fetching data:", error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchData()
+  }, [])
+
+  const columns: ColumnDef<AcademicWork>[] = [
+    {
+      accessorKey: "title",
+      header: "📖 Título",
+      cell: (info) => info.getValue(),
+    },
+    {
+      accessorKey: "publication_year",
+      header: "📅 Año",
+      cell: (info) => info.getValue(),
+    },
+    {
+      accessorKey: "cited_by_count",
+      header: "🔢 Citaciones",
+      cell: (info) => info.getValue(),
+    },
+    {
+      id: "authors",
+      header: "👥 Autores",
+      cell: ({ row }) => {
+        const rawAuthors = row?.original?.authorships.length > 2 ? row.original.authorships.slice(0, 2) : row.original.authorships
+        const authors = rawAuthors.map((a) => a.author.display_name).join(", ")
+        return <span>{authors}</span>
+      },
+    },
+  ]
+
+  if (loading) {
+    return <div className="flex items-center justify-center min-h-svh">Loading...</div>
+  }
+
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
       <InteractiveTable data={data} columns={columns} />
